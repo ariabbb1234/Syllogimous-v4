3602b586c0d2f4decff8da4ca1eb16d96754dee3
ariabbb1234 <ahmadlavasani12345@gmail.com>
2025-09-02 09:18:31 +0100

    feat: expand training unit length range

--- src/app/syllogimous/pages/settings/settings.component.ts
+++ src/app/syllogimous/pages/settings/settings.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { EnumScreens } from '../../constants/syllogimous.constants';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { DEFAULT_DAILY_GOAL, DEFAULT_PREMISES_DOWN_THRESHOLD, DEFAULT_PREMISES_UP_THRESHOLD, DEFAULT_TRAINING_UNIT_LENGTH, DEFAULT_WEEKLY_GOAL, ProgressAndPerformanceService } from '../../services/progress-and-performance.service';
 import { LS_DAILY_GOAL, LS_PREMISES_DOWN_THRESHOLD, LS_PREMISES_UP_THRESHOLD, LS_TRAINING_UNIT_LENGTH, LS_WEEKLY_GOAL } from '../../constants/local-storage.constants';
 import { SyllogimousService } from '../../services/syllogimous.service';
@@ -18,7 +18,9 @@ export class SettingsComponent {
     dailyProgressMinutes = new FormControl(DEFAULT_DAILY_GOAL);
     weeklyProgressMinutes = new FormControl(DEFAULT_WEEKLY_GOAL);
 
-    trainingUnitLength = new FormControl(DEFAULT_TRAINING_UNIT_LENGTH);
+    trainingUnitLength = new FormControl(DEFAULT_TRAINING_UNIT_LENGTH, [
+        Validators.min(1)
+    ]);
     premisesUpThreshold = new FormControl(DEFAULT_PREMISES_UP_THRESHOLD);
     premisesDownThreshold = new FormControl(DEFAULT_PREMISES_DOWN_THRESHOLD);
 
